7d8029f21ef322cdf24017ecec3243069bebc236
Rene Ramirez Vargas <renervr.8@gmail.com>
2026-04-23 00:51:15 -0600

    Carga inicial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+   imports: [IonicModule, CommonModule]
 })
 export class Tab1Page {
+
+  arboles = [
+    {
+      nombre: 'Pino',
+      descripcion: 'Este es un ejemplo del texto que tendrá el card.',
+      imagen: 'assets/pino.jpg'
+    },
+    {
+      nombre: 'Abeto',
+      descripcion: 'Otro ejemplo de descripción para el árbol.',
+      imagen: 'assets/pino.jpg'
+    }
+  ];
 
   constructor() {}
 
